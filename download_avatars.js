52ba7efc57b915d275feeecdb3a02f85977a37b6
vagrant <alvin.cl.ng@gmail.com>
2017-03-14 18:53:07 +0000

    Single File that downloads all IMG

--- download_avatars.js
+++ download_avatars.js
@@ -7,6 +7,7 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  console.log(requestURL)
   var options = {
     url: requestURL,
     headers: {
@@ -17,24 +18,54 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
       if(!error && response.statusCode == 200) {
         var data = JSON.parse(body);
+        // console.log('data', data);
 
-        var result = data.map(function(value) {
-          console.log(value.avatar_url);
-        })
+        // var result = data.map(function(value) {
+        //   return value.avatar_url
+        //   // console.log(value.avatar_url);
+        // });
+
+        const result = data.map(res => res.avatar_url);
+
 
         if (error){
-          console.log('Error, status:', response.statusCode)
+          console.log('Error, status:', response.statusCode);
         }
 
         if (response !== 200) {
-          console.log ('Correct outcome. Status code is:', response.statusCode)
+          console.log ('Correct outcome. Status code is:', response.statusCode);
         }
 
-    }
-});
+        cb(null, result)
+      }
+  });
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+  // console.log("Errors:", err);
+  // console.log("Result:", result);
+  if(!err) {
+    for(url of result) {
+      const origFileName = url.split('/')[4];
+      const newFileName = `./${origFileName}.jpg`;
+      // console.log(fileName)
+      downloadImageByURL(url, newFileName);
+    }
+  }
 });
+
+function downloadImageByURL(url, filePath) {
+  request.get(url)               // Note 1
+       .on('error', function (err) {                                   // Note 2
+         throw err;
+       })
+       .on('response', function (response) {                           // Note 3
+         console.log('Response Status Code: ', response.statusCode);
+       })
+       .pipe(fs.createWriteStream(filePath));
+}
+
+
+// downloadImageByURL();
+
+
