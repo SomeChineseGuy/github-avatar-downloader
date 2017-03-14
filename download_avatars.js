2db9baaad18a56c5b235706db7e20abd6c0c76eb
vagrant <alvin.cl.ng@gmail.com>
2017-03-14 21:01:37 +0000

    Second last one

--- download_avatars.js
+++ download_avatars.js
@@ -3,7 +3,8 @@ const GITHUB_TOKEN = "0ed5ec94f7c3e10ddd85e154ee15b5db167c597e";
 var request = require('request');
 var fs = require('fs')
 console.log('Welcome to the GitHub Avatar Downloader!');
-
+process.argv[2]
+process.argv[3]
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
@@ -18,27 +19,6 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
     if(!error && response.statusCode == 200) {
       var data = JSON.parse(body);
-
-      // const avatar_urls = data.map(res => res.avatar_url);
-      // const names = data.map(res1 => res1.login);
-
-      // // console.log(avatar_urls.length);
-      // // console.log(avatar_urls[0]);
-      // // console.log(names.length);
-      // // console.log(names[0]);
-
-      // avatar_urls = ['http://this.that.com', 'http://the.other.sh.it', ...]
-      // names = ['jeresig', 'timmywil', 'dmethvin'..];
-
-      // results = [
-      //   { url: 'blah',  name: 'jeresig'},
-      //   { url: 'blah',  name: 'jeresig'},
-      //   { url: 'blah',  name: 'jeresig'},
-      // ]
-
-
-
-
 
       if (error){
         console.log('Error, status:', response.statusCode);
@@ -56,26 +36,26 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
 getRepoContributors("jquery", "jquery", function(err, result) {
   if(!err) {
-    for(record of result) {
-      console.log(record.login + "   :   " + record.avatar_url);
-      // const origFileName = url.split('/')[4];
-      // const newFileName = `./${origFileName}.jpg`;
-      // downloadImageByURL(url, newFileName);
+    for(one of result) {
+      // console.log(one.login + "   :   " + one.avatar_url);
+      const origFileName = one.login;
+      const newFileName = `./${origFileName}.jpg`;
+      downloadImageByURL(one.avatar_url, newFileName);
     }
   }
   // console.log(name)
 });
 
-// function downloadImageByURL(url, filePath) {
-//   request.get(url)
-//   .on('error', function (err) {
-//     throw err;
-//   })
-//   .on('response', function (response) {
-//     console.log('Response Status Code: ', response.statusCode);
-//  })
-//   .pipe(fs.createWriteStream(filePath));
-// }
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+  .on('error', function (err) {
+    throw err;
+  })
+  .on('response', function (response) {
+    console.log('Response Status Code: ', response.statusCode);
+ })
+  .pipe(fs.createWriteStream(filePath));
+}
 
 
 // downloadImageByURL();
