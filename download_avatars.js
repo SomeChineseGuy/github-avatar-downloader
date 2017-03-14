2d7c3fc670a0cef20b7164e71d97941a916b9566
vagrant <alvin.cl.ng@gmail.com>
2017-03-14 04:28:13 +0000

    Got the Request to login in, next parse

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,42 @@
-var request = require('request');
+var GITHUB_USER = "SomeChineseGuy";
+var GITHUB_TOKEN = "0ed5ec94f7c3e10ddd85e154ee15b5db167c597e";
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+var request = require('request');
+
+
+
+var fs = require('fs');
+
+function getRepoContributors(repoOwner, repoName, cb) {
+  var requestURL = 'https://'+ GITHUB_USER + ':' + GITHUB_TOKEN + '@api.github.com/repos/' + repoOwner + '/' + repoName + '/contributors';
+  var options = {
+    url: requestURL,
+    headers: {
+      'User-Agent': 'request'
+    }
+  };
+  var space = "";
+  request.get(options)
+  .on('error', function (err) {
+         throw err;
+       })
+       .on('response', function (response) {
+        response.setEncoding('utf8');
+        console.log('Response Status Code: ', response.statusCode);
+        response.on('data', (data) => {
+          space = data + space;
+         });
+         response.on('end', function() {
+          console.log(space);
+        });
+        })
+
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
+
